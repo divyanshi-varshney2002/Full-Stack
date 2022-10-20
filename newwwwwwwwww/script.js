6f8cc57c87e18ab1f6fbd505ac9416ff1d1b61ad
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-10-20 11:41:27 +0530

    new

--- newwwwwwwwww/script.js
+++ newwwwwwwwww/script.js
@@ -8,6 +8,12 @@ function callback(err,data){
 fs.readFile('html.html','utf-8',callback)
 */
 
+const fs=require("fs");
+function callback(err,data){
+    if(!err){
+        console.log(data)
+    }
+}
 for(let i=0;i<=10;i++){
     fs.readFile("file"+i+".html",'utf-8',callback);
 }
