6eecbc4c01b2783a45bda4d8b133330dedcc3c92
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-03 11:55:53 +0530

    new

--- newwwwwwwwww/script.js
+++ newwwwwwwwww/script.js
@@ -73,3 +73,18 @@ p.then((data)=>{
     console.log("rejected");
 })
 */
+
+const fs=require("fs");
+const { resolve }=require("path");
+let readFilePromise=fs.promises.readFile("file1.html","utf-8");
+// console.log(readFilePromise);
+// resolve(readFilePromise)
+readFilePromise.then((data)=>{
+    console.log(data)
+})
+async function abc(){
+let data=await fs.promises.readFile("file1.html","utf-8");
+console.log(readFilePromise);
+return data;
+}
+abc();
