1124c44e3d14c7d7a4de71c0affe7ac9d72a0d75
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2022-11-16 10:07:27 +0530

    new

--- DOM/main.js
+++ DOM/main.js
@@ -1,21 +1,36 @@
-// console.log(document.all[12].innerText)
-let header=document.getElementById('header-title');
-header.style.backgroundColor='black';
-header.innerText='Good Morning.....';
-header.innerHTML='<h4>Welcome to the visual world....</h4>';
-console.log(header);
+// // console.log(document.all[12].innerText)
+// let header=document.getElementById('header-title');
+// header.style.backgroundColor='black';
+// header.innerText='Good Morning.....';
+// header.innerHTML='<h4>Welcome to the visual world....</h4>';
+// console.log(header);
 
-var items=document.getElementsByClassName("list-group-item");
-console.log(items);
-for(let i=0;i<items.length;i++){
-    // items[i].style.backgroundColor='lightgrey';
-    Object.assign(items[i].style,{backgroundColor:'lightblue',fontWeight:'bold'})
+// var items=document.getElementsByClassName("list-group-item");
+// console.log(items);
+// for(let i=0;i<items.length;i++){
+//     // items[i].style.backgroundColor='lightgrey';
+//     Object.assign(items[i].style,{backgroundColor:'lightblue',fontWeight:'bold'})
+// }
+
+// var head=document.querySelectorAll('.title');
+// for(let i=0;i<head.length;i++){
+//     head[i].style.backgroundColor='green';
+// }
+
+// var heading=document.querySelector('.title');
+// heading.style.backgroundColor='olive';
+
+var button=document.querySelector('#btn');
+button.addEventListener('click',addEvent);
+function addEvent(){
+    // console.log('1');
+    var mainTitle=document.getElementById('main-header');
+    console.log(mainTitle);
+    mainTitle.innerText='changed';
 }
-
-var head=document.querySelectorAll('.title');
-for(let i=0;i<head.length;i++){
-    head[i].style.backgroundColor='green';
+var form =document.getElementById('addForm');
+form.addEventListener('submit',runEvent);
+function runEvent(e){
+    e.preventDefault();
+    console.log('1');
 }
-
-var heading=document.querySelector('.title');
-heading.style.backgroundColor='olive';
