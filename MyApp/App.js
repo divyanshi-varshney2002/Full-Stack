25da4632a576428f8a9ec266b4ae86449fc51156
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-19 10:55:21 +0530

    newww

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
-console.log("Using Express --Divyanshi Varshney");
-app.get('/', function (req, res) {
-    res.send('hello world this is express by --Divyanshi Varshney');
-  })
-app.listen(3000);
+const port=5000;
+app.listen(port,()=>{
+  console.log(`server is running at ${port}`);
+})
+// app.use((req,res)=>{
+//   res.send(`server running`);
+// })
+app.get('/', (req, res)=>{
+  res.send('hello world this is express by --Divyanshi Varshney');
+})
+// console.log("Using Express --Divyanshi Varshney");
+// app.listen(3000);
