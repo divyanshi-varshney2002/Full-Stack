031116ca6833a869bc94ac9543d3be36f267abe9
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-19 11:48:52 +0530

    neww

--- MyApp/App.js
+++ MyApp/App.js
@@ -10,7 +10,20 @@ app.listen(port,()=>{
 app.get('/', (req, res)=>{
   res.send('<h1>Welcome to Express</h1>');
   res.send('hello world this is express by --Divyanshi Varshney');
-
+})
+app.get('/r/:subreddit', (req, res)=>{
+  console.log(req.params);//path parameter
+  const {subreddit} =req.params;
+  res.send(`<h1 style="text-align:center ; color:red">You are watching ${subreddit}</h1>`);
+  // res.send('hello world this is express by --Divyanshi Varshney');
+})
+app.get('/search',(req,res)=>{
+  console.log(req.query)//string parameter
+  const {q}=req.query;
+  res.send(`Jai shri Ram from ${q}`);
+})
+app.get('*',(req,res)=>{
+  res.send("you at wrong page !");
 })
 // console.log("Using Express --Divyanshi Varshney");
 // app.listen(3000);
