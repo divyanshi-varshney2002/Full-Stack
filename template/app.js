298ee047ad92cb791467a2ceffc9c5c5b41ea1dd
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-25 11:30:55 +0530

    new

--- template/app.js
+++ template/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const path= require("path");
 const port=3000;
 app.set('view engine','ejs');
 app.get('/home',(req,res)=>{
-    res.render('index')
+    const randomNumber=Math.floor(Math.random()*10);
+    res.render('index',{randomNumber});
 })
+app.set('views',path.join(__dirname,'views'))
 app.listen(port,()=>{
   console.log(`server is running at ${port}`);
 })
