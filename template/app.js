ac5735eda61cdb04f35e8dfac88cf82febaffe7a
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-01-25 13:55:27 +0530

    newww

--- template/app.js
+++ template/app.js
@@ -7,7 +7,12 @@ app.get('/home',(req,res)=>{
     const randomNumber=Math.floor(Math.random()*10);
     res.render('index',{randomNumber});
 })
+app.use(express.static(path.join(__dirname,'public')))
 app.set('views',path.join(__dirname,'views'))
 app.listen(port,()=>{
   console.log(`server is running at ${port}`);
+})
+const todos=["playing","reading","cooking","dancing"];
+app.get('/todos',(req,res)=>{
+  res.render('todos',{todos})
 })
