d7ea756b15007364e6635b597b76025c88d5b7d0
divyanshi-varshney2002 <divyanshivarshney2002@gmail.com>
2023-02-23 10:32:59 +0530

    nn

--- Movies-Recommendation/app.js
+++ Movies-Recommendation/app.js
@@ -1,24 +1,28 @@
-const express=require("express");
-const app=express();
-const path=require("path");
-const mongoose=require("mongoose");
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const Serie = require("./models/Serie")
 
-mongoose.connect("mongodb://localhost:27017/seriesDB")
-.then(()=>console.log("DB connected"))
-.catch((err)=>console.log(err))
+mongoose.connect("mongodb://127.0.0.1:27017/seriesDB")
+.then(()=> console.log("DB CONNECTED!"))
+.catch((err)=> console.log(err))
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
-app.use(express.static(path.join(__dirname,"public","js")))
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "public", "js")))
 
-app.get("/",(req,res)=>{
-    res.render("index");
+app.get("/", (req,res)=>{
+    res.render("index")
 })
 
-app.get("/search",(req,res)=>{
-    res.send("u r on search route")
+app.get("/search", async(req,res)=>{
+    const {q} =  req.query;
+       const series = await Serie.find({ name: { $regex : `^${q}`}});
+       console.log(series)
+      res.status(200).json(series)
 })
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000")
+app.listen(5000, ()=>{
+    console.log("server running at port 5000")
 })
--- Movies-Recommendation/public/js/script.js
+++ Movies-Recommendation/public/js/script.js
@@ -1,12 +1,27 @@
 
 // console.log("in side public script");
 
-function searchDb(){
-    fetch(`http://localhost:5000/search?q =${searchText}`)
+async function searchDb(searchText){
+    const res=await fetch(`http://localhost:5000/search?q =${searchText}`);
+    const data=await res.json()
+    console.log(data);
 }
 
 const searchInput=document.getElementById("userinput")
 searchInput.addEventListener("keyup",()=>{
     let searchText=searchInput.value;
     console.log(searchText);
+    searchDb(searchText)
 })
+
+// async function searchDb(searchText){
+//     const res = await fetch(`http://localhost:5000/search?q=${searchText}`);
+//      const data = await res.json()
+//      console.log(data);
+//  }
+ 
+//  const searchInput = document.getElementById("searchinput")
+//  searchInput.addEventListener("keyup",()=>{
+//      let searchText = searchInput.value;
+//      searchDb(searchText)
+//  })
